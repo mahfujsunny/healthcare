841c5280ad44b9aac1b8de8ca14890dbb82f10d8
Mahfuz Iqram <mahfujsunny.info@gmail.com>
2021-10-19 23:17:09 +0600

    fourth

--- src/Pages/BookDoctor/BookDoctor.js
+++ src/Pages/BookDoctor/BookDoctor.js
@@ -1,9 +1,29 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { useParams } from 'react-router';
 
 const BookDoctor = () => {
+    const {doctorid} = useParams();
+    const [book, setBook] = useState([])
+
+    useEffect(() => {
+        fetch("/data.json")
+        .then(res => res.json())
+        .then(data => setBook(data[0].doctors))
+    },[])
+
+    
+    const booking = book.find (data => parseInt(data.id) === parseInt(doctorid))
     return (
-        <div>
-         <h2>I am booking</h2>   
+        <div className="container">
+
+            <div className="">
+               { booking && <>
+                <h2>{doctorid}</h2>
+                <img src={booking?.image} alt="" />
+                <h2>{booking?.name}</h2>
+                <p>{booking?.description}</p>
+               </>}
+            </div>
         </div>
     );
 };
